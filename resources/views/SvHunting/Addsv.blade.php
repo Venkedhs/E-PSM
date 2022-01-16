9e4b18286f997445183222da206565037112ab33
Alamin468118 <82269948+Alamin468118@users.noreply.github.com>
2022-01-16 16:52:12 +0800

    ok

--- resources/views/SvHunting/Addsv.blade.php
+++ resources/views/SvHunting/Addsv.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <x-header-new />
 <!-- Content Wrapper. Contains page content -->
 <div class="content-wrapper">
@@ -115,4 +116,243 @@
 </div>
 <!-- ./wrapper -->
 
+=======
+<<<<<<< HEAD
+<x-header-new />
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Coordinator Profile</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">Utama</li>
+            <!-- <li class="breadcrumb-item active">Starter Page</li> -->
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container">
+      <table style="width:100%">
+        <tr>
+         	<center>
+			 <h1 style="text-align: center;">ADD Supervisor FORM</h1>
+        </br>
+		<button <a href="">Add Supervisor</a></button>
+        <button <a href="" target="_blank">View supervisor list </a></button>
+        <button <a href="" target="_blank">Edit supervisor list </a></button>
+
+  </center>
+	<center><fieldset>
+		     <br>
+
+			<table>
+				<form action="Addsupervisor" method="POST" name="addsupervisorForm"  >
+                @csrf
+				<center>
+				<td><td>
+					<p>
+					<td>
+					<label for="id">Sv ID:</label>
+					</td>
+					<td>
+					<input type="text" name="Sv_id" id="Sv_ID">
+					<td>
+					</p>
+				</td>
+                </tr>
+				
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_Date>Meeting Date:</label>
+					</td>
+					<td>
+					<input type="date" name="Meeting_date" id="Meeting_Date">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_Start>Start Time:</label>
+					</td>
+					<td>
+					<input type="time" name="Meeting_start" id="Meeting_Start">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_End>End Time:</label>
+					</td>
+					<td>
+					<input type="time" name="Meeting_end" id="Meeting_End">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for="Meeting_Purpose">Meeting purpose:</label>
+					</td>
+					<td>
+					<input type="text" name="Meeting_purpose" id="Meeting_Purpose">
+					</td>
+					</p>
+				</td>
+				</tr>
+			</table>
+			<center>
+			<input type="submit" name="Submit" id="Submit" value="Submit" method="post">
+            </center>
+			</p>
+			</form>
+		</fieldset>
+	</center>
+</table>
+    </div><!-- /.container -->
+  </div>
+  <!-- /.content -->
+
+</div>
+<!-- ./wrapper -->
+
+=======
+<x-header-new />
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+
+  <!-- Content Header (Page header) -->
+  <div class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1 class="m-0 text-dark">Coordinator Profile</h1>
+        </div><!-- /.col -->
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item active">Utama</li>
+            <!-- <li class="breadcrumb-item active">Starter Page</li> -->
+          </ol>
+        </div><!-- /.col -->
+      </div><!-- /.row -->
+    </div><!-- /.container-fluid -->
+  </div>
+  <!-- /.content-header -->
+
+  <!-- Main content -->
+  <div class="content">
+    <div class="container">
+      <table style="width:100%">
+        <tr>
+         	<center>
+			 <h1 style="text-align: center;">ADD SV FORM</h1>
+        </br>
+		<button <a href="">Add Meeting Booking</a></button>
+        <button <a href="" target="_blank">View Meeting Booking </a></button>
+        <button <a href="" target="_blank">Edit Meeting Booking </a></button>
+
+  </center>
+	<center><fieldset>
+		     <br>
+
+			<table>
+				<form action="AddMeetingBooking" method="POST" name="meetingForm"  >
+                @csrf
+				<center>
+				<td><td>
+					<p>
+					<td>
+					<label for="id">Student ID:</label>
+					</td>
+					<td>
+					<input type="text" name="STD_id" id="STD_ID">
+					<td>
+					</p>
+				</td>
+                </tr>
+				
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_Date>Meeting Date:</label>
+					</td>
+					<td>
+					<input type="date" name="Meeting_date" id="Meeting_Date">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_Start>Start Time:</label>
+					</td>
+					<td>
+					<input type="time" name="Meeting_start" id="Meeting_Start">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for=Meeting_End>End Time:</label>
+					</td>
+					<td>
+					<input type="time" name="Meeting_end" id="Meeting_End">
+					</td>
+					</p>
+				</td>
+				</tr>
+
+				<td><td>
+					<p>
+					<td>
+					<label for="Meeting_Purpose">Meeting purpose:</label>
+					</td>
+					<td>
+					<input type="text" name="Meeting_purpose" id="Meeting_Purpose">
+					</td>
+					</p>
+				</td>
+				</tr>
+			</table>
+			<center>
+			<input type="submit" name="Submit" id="Submit" value="Submit" method="post">
+            </center>
+			</p>
+			</form>
+		</fieldset>
+	</center>
+</table>
+    </div><!-- /.container -->
+  </div>
+  <!-- /.content -->
+
+</div>
+<!-- ./wrapper -->
+
+>>>>>>> c3e81b7b21a94424e4c3e4d6a92982636a78dd56
+>>>>>>> Stashed changes
 <x-footer-new />
--- resources/views/SvHunting/ViewSVList.blade.php
+++ resources/views/SvHunting/ViewSVList.blade.php
@@ -1,1 +1,9 @@
+<<<<<<< Updated upstream
 //users view sv
+=======
+<<<<<<< HEAD
+//users view sv
+=======
+//users view sv
+>>>>>>> c3e81b7b21a94424e4c3e4d6a92982636a78dd56
+>>>>>>> Stashed changes
