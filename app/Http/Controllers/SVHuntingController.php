02ab80595d83f164ca37e0cf05323b2f08aec163
Alamin468118 <82269948+Alamin468118@users.noreply.github.com>
2022-01-17 09:49:52 +0800

    update

--- app/Http/Controllers/SVHuntingController.php
+++ app/Http/Controllers/SVHuntingController.php
@@ -8,5 +8,10 @@ use App\Models\SvHunting;
 class studentController extends Controller
 {
     //create function to read from form 
-
+public function search()
+{
+    $search_text =$_GET['query'];
+    $SvHunting = SvHunting:: where('supervisorname','LIKE', '%'.$search_text.'%')->get();
+    $SvHunting = SvHunting:: where('expertise','LIKE', '%'.$search_text.'%')->get();
+}
 }
