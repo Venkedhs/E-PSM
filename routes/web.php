8fe95782a4d2ab3378d258b0dc8e66f913f41a7f
Alamin468118 <82269948+Alamin468118@users.noreply.github.com>
2022-01-19 11:16:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -69,45 +69,81 @@ use App\Http\Controllers\supervisorController;
 
 Route::get('supervisorprofile', [supervisorController::class, 'index']);
 Route::get('SVedit', [supervisorController::class, 'editprofile']);
-Route::post('SV_update', 'supervisorController@updateprofile');
+Route::post('SV_update', 'supervisorController@update_profile');
 // url tak boleh sama tapi ada cara kalau nak pakai url sama
 
 use App\Http\Controllers\technicianController;
 
 Route::get('technicianprofile', [technicianController::class, 'index']);
 Route::get('TECHedit', [technicianController::class, 'editprofile']);
-Route::post('TECH_update', 'technicianController@updateprofile');
+Route::post('TECH_update', 'technicianController@update_profile');
 
 use App\Http\Controllers\coordinatorController;
 
 Route::get('coordinatorprofile', [coordinatorController::class, 'index']);
 Route::get('COedit', [coordinatorController::class, 'editprofile']);
-Route::post('CO_update', 'coordinatorController@updateprofile');
+Route::post('CO_update', 'coordinatorController@update_profile');
 
 //MeetingBooking
 use App\Http\Controllers\MeetingController;
 
-Route::get('AddMeetingBooking', [MeetingController::class, 'addMeetingBooking']); //student add meeting booking
+Route::get('AddMeetingBooking', [MeetingController::class, 'MeetingInterface']);//student add meeting booking
 Route::get('ViewMeetingBooking', [MeetingController::class, 'viewMeetingBooking']); //student view meeting
 Route::get('EditMeetingBooking', [MeetingController::class, 'editMeetingBooking']); //student edit meetig booking
 Route::get('RetriveMeeting', [MeetingController::class, 'retriveMeeting']); //sv view meeting list detail
 Route::get('AddMeetingStatus', [MeetingController::class, 'addMeetingStatus']); //sv add meeting status
 
-Route::post('AddMeetingBooking', 'MeetingController@addMeetingBooking');
+Route::post('MeetingBooking', 'MeetingController@addMeetingBooking');
+
 
 //Logbook
 use App\Http\Controllers\LogbookController;
 
 Route::get('LogbookStudent', [LogbookController::class, 'logbookview']);
-Route::get('LogbookEdit', [LogbookController::class, 'logbookedit']);
-Route::get('LogbookEdit', [LogbookController::class, 'logbookedit']);
-Route::get('LogbookEdit', [LogbookController::class, 'logbookedit']);
 
 //SV Hunting
 use App\Http\Controllers\SvHuntingController;
 
-Route::get('ViewSVList',[SvHuntingController::class, 'viewSVList']); //View sv list
+Route::get('ViewSvList',[SvHuntingController::class, 'viewSVList']); //View sv list
 Route::get('/search','SvHuntingController@search');
+Route::get('EditSv', [SvHuntingController::class, 'EditSv']);
+
 Route::get('Addsv',[SvHuntingController::class, 'addsv']);
 Route::get('ApplySV', [SvHuntingController::class, 'applySV']);
 Route::get('ViewApplicationStatus',[SvHuntingController::class, 'viewApplicationStatus']);
+//Route::get('Addsv', [SvHuntingController::class, 'SvHuntingInterface']);
+//Route::get('Addsv',[SvHuntingController::class, 'addsv']);
+//Route::get('ApplySV', [SvHuntingController::class, 'applySV']);
+//Route::get('ViewApplicationStatus',[SvHuntingController::class, 'viewApplicationStatus']);
+//Route::get('apply', [SVHuntingController::class, 'applySV']);
+
+// manage inventory
+use App\Http\Controllers\ManageInventoryController;
+
+Route::group(['prefix' => 'manage-inventory', 'as' => 'manage-inventory.'], function (){
+    Route::get('/items', [ManageInventoryController::class,'viewAll'])
+        ->name('view-all');
+    Route::get('/my-items', [ManageInventoryController::class, 'viewOne'])
+        ->name('view-one');
+
+    Route::post('/add', [ManageInventoryController::class, 'store'])
+        ->name('store');
+
+    Route::get('/show/{item_id}', [ManageInventoryController::class, 'show'])
+        ->name('show');
+
+    Route::get('/edit/{item_id}', [ManageInventoryController::class, 'edit'])
+        ->name('edit');
+    Route::post('/update/{item_id}', [ManageInventoryController::class, 'update'])
+        ->name('update');
+    Route::get('/delete/{item_id}', [ManageInventoryController::class, 'destroy'])
+        ->name('destroy');
+
+    Route::post('/approval/{item_id}', [ManageInventoryController::class, 'setApproval'])
+        ->name('approval');
+});
+
+
+
+
+
