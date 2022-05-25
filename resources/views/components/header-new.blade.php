92343429897d0469586bdc80543580036f50bbaf
saifultech <saifultech.official@gmail.com>
2022-05-25 22:06:28 +0800

    25/05/2022 10pm

--- resources/views/components/header-new.blade.php
+++ resources/views/components/header-new.blade.php
@@ -117,7 +117,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 
 							<ul class="nav nav-treeview  ">
 								<li class="nav-item ">
-									<a href="ViewSvList" class="nav-link  @if(url()->current() ===  URL::to('/ViewSvList'))
+									<a href="/ViewSvList" class="nav-link  @if(url()->current() ===  URL::to('/ViewSvList'))
 								active
 								@endif">
 										<i class="fas fa-angle-right nav-icon"></i>
@@ -133,7 +133,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								</li> --}}
 
 								<li class="nav-item">
-									<a href="/manage-title/view" class="nav-link @if(url()->current() ===  URL::to('/manage-title/view'))
+									<a href="/manage-title/my-title" class="nav-link @if(url()->current() ===  URL::to('/manage-title/my-title'))
 								active
 						   @endif">
 										<i class="fas fa-angle-right nav-icon"></i>
@@ -224,11 +224,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								<li class="nav-item">
 									<a href="/manage-title/my-title-sv" class="nav-link ">
 										<i class="fas fa-angle-right nav-icon"></i>
-<<<<<<< Updated upstream
-										<p>2.0 Title selection</p>
-=======
 										<p>1.0 Proposal</p>
->>>>>>> Stashed changes
 									</a>
 								</li>
 
@@ -237,11 +233,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								active
 						   @endif">
 										<i class="fas fa-angle-right nav-icon"></i>
-<<<<<<< Updated upstream
-										<p>3.0 Logbook</p>
-=======
 										<p>2.0 Logbook</p>
->>>>>>> Stashed changes
 									</a>
 								</li>
 
@@ -250,11 +242,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
 								active
 						   @endif">
 										<i class="fas fa-angle-right nav-icon"></i>
-<<<<<<< Updated upstream
-										<p>4.0 Meeting</p>
-=======
 										<p>3.0 Meeting</p>
->>>>>>> Stashed changes
 									</a>
 								</li>
 							</ul>
